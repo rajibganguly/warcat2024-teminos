93f148ebecc17ec09381e51d87bfb65f2a8c9d09
Soumen <105904286+ok-soumen@users.noreply.github.com>
2024-05-17 13:44:02 +0530

    remove token as for now

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -23,30 +23,30 @@ const upload = multer({ storage });
 // Enable CORS for all routes
 router.use(cors());
 // Define routes
-router.post('/register-user-with-department',authMiddleware, departmentController.registerUserWithDepartment);
-router.put('/edit-register-user-with-department', authMiddleware, departmentController.editRegisterUserWithDepartment);
-router.get('/departments', authMiddleware, departmentController.getAllDepartments);
+router.post('/register-user-with-department', departmentController.registerUserWithDepartment);
+router.put('/edit-register-user-with-department',  departmentController.editRegisterUserWithDepartment);
+router.get('/departments',  departmentController.getAllDepartments);
 router.post('/login', userController.loginUser);
-router.get('/profile', authMiddleware, userController.getProfile);
+router.get('/profile',  userController.getProfile);
 router.get('/logout', userController.logoutUser); // Added authMiddleware here
 router.post('/reset-password', userController.resetPassword); // Added authMiddleware here
 router.post('/add-meeting', upload.single('file'), meetingController.addMeeting); // Define route for adding a meeting
-router.put('/edit-meeting', upload.single('file'), authMiddleware, meetingController.editMeeting);
+router.put('/edit-meeting', upload.single('file'),  meetingController.editMeeting);
 router.get('/meetings', meetingController.getAllMeetings);
 router.post('/add-task', upload.array('task_image', 30), taskController.addTask);
-router.get('/tasks', authMiddleware, taskController.getTask);
-router.post('/edit-task', upload.single('task_image'), authMiddleware, taskController.editTask);
-router.post('/add-sub-task', upload.single('subtask_image'),authMiddleware, taskController.addSubTask);
-router.post('/edit-sub-task', upload.single('subtask_image'), authMiddleware, taskController.editSubTask);
-router.post('/add-meeting', authMiddleware, meetingController.addMeeting); // Define route for adding a meeting
-router.put('/edit-meeting', authMiddleware, meetingController.editMeeting);
-router.get('/meetings',authMiddleware, meetingController.getAllMeetings);
-router.delete('/deleteDepartment/:departmentId', authMiddleware, departmentController.deleteDepartment);
-router.post('/tasks/:taskId/add-note',authMiddleware, taskController.addNoteToTask);
-router.post('/tasks/:taskId/upload-completion-details', upload.single('upload_report'),authMiddleware, taskController.uploadCompletionDetails);
+router.get('/tasks', taskController.getTask);
+router.post('/edit-task', upload.single('task_image'), taskController.editTask);
+router.post('/add-sub-task', upload.single('subtask_image'), taskController.addSubTask);
+router.post('/edit-sub-task', upload.single('subtask_image'), taskController.editSubTask);
+router.post('/add-meeting', meetingController.addMeeting); // Define route for adding a meeting
+router.put('/edit-meeting', meetingController.editMeeting);
+router.get('/meetings', meetingController.getAllMeetings);
+router.delete('/deleteDepartment/:departmentId', departmentController.deleteDepartment);
+router.post('/tasks/:taskId/add-note', taskController.addNoteToTask);
+router.post('/tasks/:taskId/upload-completion-details', upload.single('upload_report'), taskController.uploadCompletionDetails);
 router.get('/task-status-percentages',taskController.getTaskStatusPercentages);
-router.put('/admin_verified', authMiddleware, taskController.setAdminVerified);
-router.get('/statistics', authMiddleware, getStatistics);
+router.put('/admin_verified', taskController.setAdminVerified);
+router.get('/statistics', getStatistics);
 
 
 
